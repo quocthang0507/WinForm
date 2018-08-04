d2ea4e3a41686309fb7f5ddc8ce99c269e2336b3
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2018-08-04 17:30:31 +0700

    Add only a instance mode

--- ChuongTrinhThiTracNghiem/ChuongTrinhThiTracNghiem/Program.cs
+++ ChuongTrinhThiTracNghiem/ChuongTrinhThiTracNghiem/Program.cs
@@ -6,17 +6,25 @@ using System.Windows.Forms;
 
 namespace ChuongTrinhThiTracNghiem
 {
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form_Question());
-        }
-    }
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			bool result;
+			var mutex = new Mutex(true, "UniqueAppId", out result);
+			if (!result)
+			{
+				MessageBox.Show("Ứng dụng đang chạy!", "Thông báo!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return;
+			}
+			Application.Run(new Form_Question());
+			GC.KeepAlive(mutex);
+		}
+	}
 }
