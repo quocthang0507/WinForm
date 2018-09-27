5a201e49ed627b6884a85e182f843a0e7ba9569a
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2018-09-27 10:48:59 +0700

    Upload new project

---
+++ ConvertIP/ConvertIP/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace ConvertIP
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.txtSubnet = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtIP = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.txtBroadcast = new System.Windows.Forms.TextBox();
+			this.txtNetwork = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.btnConvert = new System.Windows.Forms.Button();
+			this.btnReset = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.panel1.Controls.Add(this.txtSubnet);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.txtIP);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(327, 62);
+			this.panel1.TabIndex = 0;
+			// 
+			// txtSubnet
+			// 
+			this.txtSubnet.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.txtSubnet.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+			this.txtSubnet.Location = new System.Drawing.Point(143, 33);
+			this.txtSubnet.MaxLength = 2;
+			this.txtSubnet.Name = "txtSubnet";
+			this.txtSubnet.Size = new System.Drawing.Size(72, 22);
+			this.txtSubnet.TabIndex = 2;
+			this.txtSubnet.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSubnet_KeyPress);
+			this.txtSubnet.Leave += new System.EventHandler(this.txtSubnet_Leave);
+			// 
+			// label2
+			// 
+			this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+			this.label2.Location = new System.Drawing.Point(8, 36);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(86, 16);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Subnet mask";
+			// 
+			// txtIP
+			// 
+			this.txtIP.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.txtIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+			this.txtIP.Location = new System.Drawing.Point(143, 3);
+			this.txtIP.MaxLength = 15;
+			this.txtIP.Name = "txtIP";
+			this.txtIP.Size = new System.Drawing.Size(165, 22);
+			this.txtIP.TabIndex = 1;
+			this.txtIP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtIP_KeyPress);
+			this.txtIP.Leave += new System.EventHandler(this.txtIP_Leave);
+			// 
+			// label1
+			// 
+			this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+			this.label1.Location = new System.Drawing.Point(8, 6);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(74, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "IP Address";
+			// 
+			// panel2
+			// 
+			this.panel2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.panel2.Controls.Add(this.txtBroadcast);
+			this.panel2.Controls.Add(this.txtNetwork);
+			this.panel2.Controls.Add(this.label4);
+			this.panel2.Controls.Add(this.label3);
+			this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+			this.panel2.Location = new System.Drawing.Point(12, 109);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(327, 70);
+			this.panel2.TabIndex = 1;
+			// 
+			// txtBroadcast
+			// 
+			this.txtBroadcast.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.txtBroadcast.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+			this.txtBroadcast.Location = new System.Drawing.Point(143, 37);
+			this.txtBroadcast.MaxLength = 15;
+			this.txtBroadcast.Name = "txtBroadcast";
+			this.txtBroadcast.ReadOnly = true;
+			this.txtBroadcast.Size = new System.Drawing.Size(165, 22);
+			this.txtBroadcast.TabIndex = 6;
+			// 
+			// txtNetwork
+			// 
+			this.txtNetwork.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.txtNetwork.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+			this.txtNetwork.Location = new System.Drawing.Point(143, 7);
+			this.txtNetwork.MaxLength = 15;
+			this.txtNetwork.Name = "txtNetwork";
+			this.txtNetwork.ReadOnly = true;
+			this.txtNetwork.Size = new System.Drawing.Size(165, 22);
+			this.txtNetwork.TabIndex = 5;
+			// 
+			// label4
+			// 
+			this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+			this.label4.Location = new System.Drawing.Point(8, 40);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(124, 16);
+			this.label4.TabIndex = 2;
+			this.label4.Text = "Broadcast Address";
+			// 
+			// label3
+			// 
+			this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+			this.label3.Location = new System.Drawing.Point(8, 10);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(111, 16);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "Network Address";
+			// 
+			// btnConvert
+			// 
+			this.btnConvert.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.btnConvert.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+			this.btnConvert.Location = new System.Drawing.Point(92, 80);
+			this.btnConvert.Name = "btnConvert";
+			this.btnConvert.Size = new System.Drawing.Size(75, 23);
+			this.btnConvert.TabIndex = 3;
+			this.btnConvert.Text = "Convert!";
+			this.btnConvert.UseVisualStyleBackColor = true;
+			this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+			// 
+			// btnReset
+			// 
+			this.btnReset.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+			this.btnReset.Location = new System.Drawing.Point(184, 80);
+			this.btnReset.Name = "btnReset";
+			this.btnReset.Size = new System.Drawing.Size(75, 23);
+			this.btnReset.TabIndex = 4;
+			this.btnReset.Text = "Reset!";
+			this.btnReset.UseVisualStyleBackColor = true;
+			this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+			// 
+			// Form1
+			// 
+			this.AcceptButton = this.btnConvert;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.CancelButton = this.btnReset;
+			this.ClientSize = new System.Drawing.Size(351, 184);
+			this.Controls.Add(this.btnReset);
+			this.Controls.Add(this.btnConvert);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Chuyển đổi IP";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TextBox txtSubnet;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtIP;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.TextBox txtBroadcast;
+		private System.Windows.Forms.TextBox txtNetwork;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button btnConvert;
+		private System.Windows.Forms.Button btnReset;
+	}
+}
+
---
+++ ConvertIP/ConvertIP_2/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace ConvertIP_2
+{
+	public partial class Form1 : Form
+	{
+		string ip;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		int Dec2Bin(int dec)
+		{
+			return int.Parse(Convert.ToString(dec, 2));
+		}
+
+		int Bin2Dec(string bin)
+		{
+			return Convert.ToInt32(bin, 2);
+		}
+
+		bool CheckValidIP(string ip)
+		{
+			IPAddress ipAddress;
+			if (IPAddress.TryParse(ip, out ipAddress))
+				return true;
+			return false;
+		}
+
+		bool CheckSubnetMask(int n)
+		{
+			return n <= 32 && n >= 0;
+		}
+
+		string DecStr2BinStr(string s)
+		{
+			string[] t = s.Split('.');
+			string bin = "";
+			foreach (var item in t)
+				bin += Dec2Bin(int.Parse(item)).ToString("D8");
+			return bin;
+		}
+
+		string BinStr2DecStr(string s)
+		{
+			string dec = "";
+			int i = 0;
+			do
+			{
+				dec += Bin2Dec(s.Substring(i * 8, 8)).ToString();
+				if (i < 3)
+					dec += ".";
+				i++;
+			} while (i < 4);
+			return dec;
+		}
+
+		private void txtSubnet_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (e.KeyChar >= 48 && e.KeyChar <= 57 || e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Delete)
+				e.Handled = false;
+			else e.Handled = true;
+		}
+
+		private void txtIP_Leave(object sender, EventArgs e)
+		{
+			if (!btnReset.Focused)
+				if (txtIP.Text != "")
+					if (!CheckValidIP(ip = txtIP.Text.Replace(',', '.').Replace(" ", "")))
+					{
+						MessageBox.Show("Invalid IP address!", "Invalid IP", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+						txtIP.Focus();
+					}
+		}
+
+		private void txtSubnet_Leave(object sender, EventArgs e)
+		{
+			if (txtSubnet.Text != "")
+				if (!CheckSubnetMask(int.Parse(txtSubnet.Text)))
+				{
+					MessageBox.Show("Invalid subnet mask!", "Invalid subnet mask", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+					txtSubnet.Focus();
+				}
+		}
+
+		string[] ConvertIt(string IP, string mask)
+		{
+			string[] result = new string[2];
+			string s = DecStr2BinStr(IP);
+			string s_network = s.Substring(0, int.Parse(mask));
+			string s_host = s.Substring(int.Parse(mask));
+			string s_host_network = s_host.Replace('1', '0');
+			string s_host_broadcast = s_host.Replace('0', '1');
+			result[0] = BinStr2DecStr(s_network + s_host_network);
+			result[1] = BinStr2DecStr(s_network + s_host_broadcast);
+			return result;
+		}
+
+		private void btnConvert_Click(object sender, EventArgs e)
+		{
+			if (txtIP.Text != "" && txtSubnet.Text != "")
+			{
+				string[] t = ConvertIt(ip, txtSubnet.Text);
+				txtNetwork.Text = t[0];
+				txtBroadcast.Text = t[1];
+			}
+			else if (txtIP.Text == "")
+				txtIP.Focus();
+			else txtSubnet.Focus();
+		}
+
+		private void btnReset_Click(object sender, EventArgs e)
+		{
+			txtIP.Clear();
+			txtSubnet.Clear();
+			txtNetwork.Clear();
+			txtBroadcast.Clear();
+			txtIP.Focus();
+		}
+
+		string FindFile()
+		{
+			OpenFileDialog open_file = new OpenFileDialog();
+			open_file.InitialDirectory = Directory.GetCurrentDirectory();
+			open_file.Title = "Duyệt đến tập tin";
+			open_file.Filter = "Text file|*.txt";
+			open_file.RestoreDirectory = true;
+			DialogResult r = open_file.ShowDialog();
+			if (r == DialogResult.OK)
+			{
+				string file_path = open_file.FileName;
+				file_path = new FileInfo(file_path).FullName;
+				return file_path;
+			}
+			else
+				return null;
+		}
+
+		string FixedWidth(string s, int length)
+		{
+			return String.Format("{0,-" + length + "}", s);
+		}
+
+		private void btnFile_Click(object sender, EventArgs e)
+		{
+			string path = FindFile();
+			if (path == null)
+				MessageBox.Show("You haven't chosen file text yet", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			else
+			{
+				StreamReader sr = new StreamReader(path);
+				string p2 = path.Replace(".txt", "_answered.txt");
+				StreamWriter sw = new StreamWriter(p2);
+				string line;
+				sw.WriteLine(FixedWidth("IP_Addresses/Subnetmask", 25) + FixedWidth("Network_Addresses", 25) + FixedWidth("Broadcast_Addresses", 25));
+				while ((line = sr.ReadLine()) != null)
+				{
+					if (line.Contains("/"))
+					{
+						string[] temp = line.Split('/');
+						temp[0] = temp[0].Replace("\t", "");
+						if (CheckValidIP(temp[0]))
+						{
+							string[] t = ConvertIt(temp[0], temp[1]);
+							sw.WriteLine(FixedWidth(temp[0] + "/" + temp[1], 25) + FixedWidth(t[0], 25) + FixedWidth(t[1], 25));
+						}
+					}
+				}
+				sr.Close();
+				sw.Close();
+				Process.Start(p2);
+			}
+		}
+	}
+}
