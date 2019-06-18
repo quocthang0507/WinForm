5d7f9ba5f0b47915c328bab4021886832ee6d646
La Quốc Thắng <37357028+laquocthang@users.noreply.github.com>
2019-06-18 19:58:51 +0700

    Upload new project

---
+++ Hook/Hook/HookLib/RawKeyEventArgs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Hook.HookLib
+{
+	/// <summary>
+	/// Transmission info into Keyboard event
+	/// </summary>
+	public class RawKeyEventArgs : EventArgs
+	{
+		/// <summary>
+		/// VKCode of the key
+		/// </summary>
+		public int VKCode;
+
+		/// <summary>
+		/// WPF Key of the key
+		/// </summary>
+		public Key Key;
+
+		/// <summary>
+		/// Is the hitted key system key
+		/// </summary>
+		public bool IsSysKey;
+
+		/// <summary>
+		/// Create raw keyvent arguments
+		/// </summary>
+		/// <param name="VKCode"></param>
+		/// <param name="isSyskey"></param>
+		public RawKeyEventArgs(int VKCode, bool isSyskey)
+		{
+			this.VKCode = VKCode;
+			this.IsSysKey = isSyskey;
+			this.Key = KeyInterop.KeyFromVirtualKey(VKCode);
+		}
+	}
+}
---
+++ Hook/Hook/HookLib/UsingHookKey.cs
@@ -0,0 +1,24 @@
+﻿namespace Hook.HookLib
+{
+	class UsingHookKey
+	{
+		KeyboardListener keyboardListener;
+
+		public UsingHookKey()
+		{
+			keyboardListener = new KeyboardListener();
+			keyboardListener.KeyDown += keyboardListener_KeyDown;
+		}
+
+		private void keyboardListener_KeyDown(object sender, RawKeyEventArgs args)
+		{
+			switch (args.VKCode)
+			{
+				case VKCodeConst.A:
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
