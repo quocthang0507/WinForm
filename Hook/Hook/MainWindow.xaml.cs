37c9f9f2d7bddf168ac80f7bc8200621a84e2923
La Quốc Thắng <37357028+laquocthang@users.noreply.github.com>
2019-06-20 10:13:28 +0700

    Complete the function

--- Hook/Hook/MainWindow.xaml.cs
+++ Hook/Hook/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Hook
 {
@@ -20,9 +9,30 @@ namespace Hook
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private HookLib.UsingHookKey usingHook;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Grid_Loaded(object sender, RoutedEventArgs e)
+		{
+			usingHook = new HookLib.UsingHookKey();
+		}
+
+		/// <summary>
+		/// Show the message on the textbox
+		/// Used for calling the non-static object in the static method
+		/// </summary>
+		/// <param name="message"></param>
+		public static void Show(string message)
+		{
+			Application.Current.Dispatcher.Invoke(new Action(() =>
+			{
+				MainWindow my = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+				my.showResult.Text += message + "\n";
+			}));
 		}
 	}
 }
