ded93f73ac3447a5d7ec625e5d0dddc48a8f99b5
La Quốc Thắng <37357028+laquocthang@users.noreply.github.com>
2019-06-21 17:47:27 +0700

    Upload OCR project

---
+++ OCR_Vietnamese/OCR_Vietnamese/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace OCR_Vietnamese
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_chonfile = new System.Windows.Forms.Button();
+            this.btn_ocr = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.picloading = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picloading)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_chonfile
+            // 
+            this.btn_chonfile.Location = new System.Drawing.Point(358, 44);
+            this.btn_chonfile.Name = "btn_chonfile";
+            this.btn_chonfile.Size = new System.Drawing.Size(102, 23);
+            this.btn_chonfile.TabIndex = 1;
+            this.btn_chonfile.Text = "Chọn hình ảnh...";
+            this.btn_chonfile.UseVisualStyleBackColor = true;
+            this.btn_chonfile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_ocr
+            // 
+            this.btn_ocr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ocr.ForeColor = System.Drawing.Color.Red;
+            this.btn_ocr.Location = new System.Drawing.Point(475, 155);
+            this.btn_ocr.Name = "btn_ocr";
+            this.btn_ocr.Size = new System.Drawing.Size(102, 89);
+            this.btn_ocr.TabIndex = 2;
+            this.btn_ocr.Text = "OCR";
+            this.btn_ocr.UseVisualStyleBackColor = true;
+            this.btn_ocr.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.richTextBox1.Location = new System.Drawing.Point(590, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(486, 467);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hình ảnh";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(587, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Kết quả";
+            // 
+            // picloading
+            // 
+            this.picloading.Image = global::OCR_Vietnamese.Properties.Resources.DefensiveMiserlyCopperbutterfly_size_restricted;
+            this.picloading.Location = new System.Drawing.Point(606, 138);
+            this.picloading.Name = "picloading";
+            this.picloading.Size = new System.Drawing.Size(107, 106);
+            this.picloading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picloading.TabIndex = 7;
+            this.picloading.TabStop = false;
+            this.picloading.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::OCR_Vietnamese.Properties.Resources.logo;
+            this.pictureBox2.Location = new System.Drawing.Point(487, 82);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(90, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(448, 467);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 552);
+            this.Controls.Add(this.picloading);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn_ocr);
+            this.Controls.Add(this.btn_chonfile);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "[C#] Trích xuất chữ từ hình ảnh (OCR Việt Nam) - https://laptrinhvb.net";
+            ((System.ComponentModel.ISupportInitialize)(this.picloading)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_chonfile;
+        private System.Windows.Forms.Button btn_ocr;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox picloading;
+    }
+}
+
---
+++ OCR_Vietnamese/OCR_Vietnamese/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace OCR_Vietnamese
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.LoadAsync(openFileDialog.FileName);
+            }        }
+
+    
+
+        private string OCR(Bitmap b)
+        {
+            string res = "";
+            using (var engine = new TesseractEngine(@"tessdata", "vie", EngineMode.Default))
+            {  
+                using (var page = engine.Process(b, PageSegMode.AutoOnly))
+                    res = page.GetText();
+            }
+            return res;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            Task.Factory.StartNew(() => {
+                picloading.BeginInvoke(new Action(() =>
+                {                    picloading.Visible = true;
+                }));
+
+                result = OCR((Bitmap)pictureBox1.Image);
+                richTextBox1.BeginInvoke(new Action(() => {
+
+                    richTextBox1.Text = result;
+
+                }));
+                picloading.BeginInvoke(new Action(() =>
+                {
+                    picloading.Visible = false;
+                }));
+
+            });
+        }
+    }
+}
