4796db832b33c75f731e33da55df31a451168bb0
La Quốc Thắng <37357028+laquocthang@users.noreply.github.com>
2019-07-08 22:06:47 +0700

    Upload new project

---
+++ Search/Search/Search.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Search
+{
+	partial class Search
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.tbx_location = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+			this.btn_load = new System.Windows.Forms.Button();
+			this.btn_open = new System.Windows.Forms.Button();
+			this.dgv_list = new System.Windows.Forms.DataGridView();
+			this.col_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.col_Location = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.col_CreationDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.col_Size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.tableLayoutPanel3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgv_list)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.dgv_list);
+			this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+			this.splitContainer1.SplitterDistance = 100;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 100);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 1;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.Controls.Add(this.tbx_location, 0, 1);
+			this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 2);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 3;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(234, 100);
+			this.tableLayoutPanel2.TabIndex = 0;
+			// 
+			// tbx_location
+			// 
+			this.tbx_location.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tbx_location.Location = new System.Drawing.Point(3, 36);
+			this.tbx_location.Name = "tbx_location";
+			this.tbx_location.Size = new System.Drawing.Size(228, 20);
+			this.tbx_location.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 0);
+			this.label1.Name = "label1";
+			this.label1.Padding = new System.Windows.Forms.Padding(5);
+			this.label1.Size = new System.Drawing.Size(61, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Location:";
+			// 
+			// tableLayoutPanel3
+			// 
+			this.tableLayoutPanel3.ColumnCount = 2;
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel3.Controls.Add(this.btn_load, 1, 0);
+			this.tableLayoutPanel3.Controls.Add(this.btn_open, 0, 0);
+			this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 69);
+			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+			this.tableLayoutPanel3.RowCount = 1;
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(228, 28);
+			this.tableLayoutPanel3.TabIndex = 2;
+			// 
+			// btn_load
+			// 
+			this.btn_load.Location = new System.Drawing.Point(117, 3);
+			this.btn_load.Name = "btn_load";
+			this.btn_load.Size = new System.Drawing.Size(75, 22);
+			this.btn_load.TabIndex = 2;
+			this.btn_load.Text = "Load";
+			this.btn_load.UseVisualStyleBackColor = true;
+			this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+			// 
+			// btn_open
+			// 
+			this.btn_open.Location = new System.Drawing.Point(3, 3);
+			this.btn_open.Name = "btn_open";
+			this.btn_open.Size = new System.Drawing.Size(75, 22);
+			this.btn_open.TabIndex = 3;
+			this.btn_open.Text = "Open";
+			this.btn_open.UseVisualStyleBackColor = true;
+			this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+			// 
+			// dgv_list
+			// 
+			this.dgv_list.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dgv_list.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgv_list.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col_Name,
+            this.col_Location,
+            this.col_CreationDate,
+            this.col_Size});
+			this.dgv_list.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dgv_list.Location = new System.Drawing.Point(0, 0);
+			this.dgv_list.Name = "dgv_list";
+			this.dgv_list.Size = new System.Drawing.Size(800, 346);
+			this.dgv_list.TabIndex = 0;
+			// 
+			// col_Name
+			// 
+			this.col_Name.HeaderText = "Name";
+			this.col_Name.Name = "col_Name";
+			// 
+			// col_Location
+			// 
+			this.col_Location.HeaderText = "Location";
+			this.col_Location.Name = "col_Location";
+			// 
+			// col_CreationDate
+			// 
+			this.col_CreationDate.HeaderText = "Creation Date";
+			this.col_CreationDate.Name = "col_CreationDate";
+			// 
+			// col_Size
+			// 
+			this.col_Size.HeaderText = "Size";
+			this.col_Size.Name = "col_Size";
+			// 
+			// Search
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.splitContainer1);
+			this.Name = "Search";
+			this.Text = "Windows Search";
+			this.Load += new System.EventHandler(this.Search_Load);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.tableLayoutPanel2.PerformLayout();
+			this.tableLayoutPanel3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dgv_list)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox tbx_location;
+		private System.Windows.Forms.Button btn_load;
+		private System.Windows.Forms.Button btn_open;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+		private System.Windows.Forms.DataGridView dgv_list;
+		private System.Windows.Forms.DataGridViewTextBoxColumn col_Name;
+		private System.Windows.Forms.DataGridViewTextBoxColumn col_Location;
+		private System.Windows.Forms.DataGridViewTextBoxColumn col_CreationDate;
+		private System.Windows.Forms.DataGridViewTextBoxColumn col_Size;
+	}
+}
+
---
+++ Search/Search/Search.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Search
+{
+	public partial class Search : Form
+	{
+		public Search()
+		{
+			InitializeComponent();
+			dgv_list.RowHeadersVisible = false;
+		}
+
+		private void Search_Load(object sender, EventArgs e)
+		{
+			splitContainer1.FixedPanel = FixedPanel.Panel1;
+		}
+
+		private void btn_open_Click(object sender, EventArgs e)
+		{
+			using (var dialog = new FolderBrowserDialog())
+			{
+				DialogResult result = dialog.ShowDialog();
+				if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+				{
+					tbx_location.Text = dialog.SelectedPath;
+				}
+			}
+		}
+
+		private void btn_load_Click(object sender, EventArgs e)
+		{
+			getListFolders(tbx_location.Text);
+			getListFiles(tbx_location.Text);
+		}
+
+		private void getListFolders(string path)
+		{
+			DirectoryInfo directory = new DirectoryInfo(path);
+			DirectoryInfo[] list = directory.GetDirectories();
+			object[] row;
+			foreach (var item in list)
+			{
+				row = new object[4];
+				row[0] = item.Name;
+				row[1] = item.FullName;
+				row[2] = item.CreationTime;
+				row[3] = convertUnit(getSizeDir(item.FullName));
+				dgv_list.Rows.Add(row);
+			}
+		}
+
+		private void getListFiles(string path)
+		{
+			DirectoryInfo directory = new DirectoryInfo(path);
+			FileInfo[] list = directory.GetFiles();
+			object[] row;
+			foreach (var item in list)
+			{
+				row = new object[4];
+				row[0] = item.Name;
+				row[1] = item.FullName;
+				row[2] = item.CreationTime;
+				row[3] = item.Length;
+				dgv_list.Rows.Add(row);
+			}
+		}
+
+		private double getSizeDir(string path)
+		{
+			double size = 0;
+			DirectoryInfo directory = new DirectoryInfo(path);
+			DirectoryInfo[] subDirectory = directory.GetDirectories();
+			var filter = subDirectory.Where(f => !f.Attributes.HasFlag(FileAttributes.Hidden));
+			foreach (var item in filter)
+			{
+				size += getSizeDir(item.FullName);
+			}
+			size += directory.EnumerateFiles().Sum(f => f.Length);
+			return size;
+		}
+
+		private string[] unit = { "B", "KB", "MB", "GB", "TB" };
+
+		private string convertUnit(double size)
+		{
+			int order = 0;
+			while (size >= 1024 && order < unit.Length - 1)
+			{
+				order++;
+				size /= 1024;
+			}
+			return string.Format("{0:0.##} {1}", size, unit[order]);
+		}
+	}
+}
