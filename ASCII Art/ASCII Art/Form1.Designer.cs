9ef5c75ccc766e5209429fbbfeeb82b33588d115
La Quốc Thắng <37357028+laquocthang@users.noreply.github.com>
2019-07-31 16:44:41 +0700

    Upload new project

---
+++ ASCII Art/ASCII Art/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ASCII_Art
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_browser = new System.Windows.Forms.Button();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(374, 380);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_browser
+            // 
+            this.btn_browser.Location = new System.Drawing.Point(257, 12);
+            this.btn_browser.Name = "btn_browser";
+            this.btn_browser.Size = new System.Drawing.Size(145, 59);
+            this.btn_browser.TabIndex = 1;
+            this.btn_browser.Text = "Chọn hình...";
+            this.btn_browser.UseVisualStyleBackColor = true;
+            this.btn_browser.Click += new System.EventHandler(this.btn_browser_Click);
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Location = new System.Drawing.Point(408, 12);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(150, 59);
+            this.btn_convert.TabIndex = 2;
+            this.btn_convert.Text = "Convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(417, 86);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(442, 380);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ASCII_Art.Properties.Resources.logo;
+            this.pictureBox2.Location = new System.Drawing.Point(778, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(82, 76);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 480);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btn_convert);
+            this.Controls.Add(this.btn_browser);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "[C#] Convert Picture to Image Art - https://laptrinhvb.net";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_browser;
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ ASCII Art/ASCII Art/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASCII_Art
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string filename;
+
+        private void btn_browser_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new OpenFileDialog())
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    filename = dlg.FileName;
+                    pictureBox1.Image = Image.FromFile(filename);
+                }
+            }
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            Image img = Image.FromFile(filename);
+            Bitmap bmp = new Bitmap(img, 100, 100);
+            show_image(convert_image(bmp));
+        }
+
+        private unsafe StringBuilder convert_image(Bitmap bmp)
+        {
+            StringBuilder asciiResult = new StringBuilder();
+            asciiResult.Append("");
+
+            int bmpHeight = bmp.Height;
+            int bmpWidth = bmp.Width;
+
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmpWidth, bmpHeight), ImageLockMode.ReadOnly, bmp.PixelFormat);
+
+            int bmpStride = bmpData.Stride;
+            byte* currentPixel = (byte*)bmpData.Scan0;
+
+            for (int y = 0; y < bmpHeight; y++)
+            {
+                for (int x = 0; x < bmpWidth; x++)
+                {
+                    int r = currentPixel[x * 4];
+                    int g = currentPixel[x * 4 + 1];
+                    int b = currentPixel[x * 4 + 2];
+                    int alpha = currentPixel[x * 4 + 3];
+                     asciiResult.Append($@"<span style='color: rgb({r},{ g},{ b}); '>{getAsciiChar(alpha)}</span>");
+
+                }
+                currentPixel += bmpStride;
+                asciiResult.Append("<br>");
+
+            }
+            asciiResult.Append("<br>");
+
+            bmp.UnlockBits(bmpData);
+            return asciiResult;
+        }
+
+        private char getAsciiChar(int alpha)
+        {
+            if (alpha >= 240)
+                return '@';
+            if (alpha >= 200)
+                return '#';
+            if (alpha >= 160)
+                return '$';
+            if (alpha >= 120)
+                return '%';
+            if (alpha >= 80)
+                return '8';
+            if (alpha >= 40)
+                return '|';
+
+            return '.';
+        }
+        private void show_image(StringBuilder asciiResult)
+        {
+            StreamWriter sw = new StreamWriter("image.html");
+            sw.Write(asciiResult.ToString());
+            sw.Close();
+
+            string html = asciiResult.ToString();
+            webBrowser1.DocumentText = html;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser1.Document.Body.Style = "zoom:20%;";
+        }
+    }
+}
+
