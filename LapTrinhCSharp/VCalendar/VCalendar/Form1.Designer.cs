1e11b609a46396536fa507258ee4d46404fe9419
La Quốc Thắng <quocthang0507@gmail.com>
2020-01-29 10:56:23 +0700

    Upload new project

---
+++ LapTrinhCSharp/VCalendar/VCalendar/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace VCalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(28, 37);
+			this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(458, 21);
+			this.dateTimePicker1.TabIndex = 0;
+			this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(371, 96);
+			this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(116, 27);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "GET";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+			this.label1.Location = new System.Drawing.Point(24, 144);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(192, 20);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Không có gì để hiển thị";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(528, 315);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "Form1";
+			this.Text = "[C#] Solar to Lunnar Calendar";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LapTrinhCSharp/VCalendar/VCalendar/Form1.cs
@@ -0,0 +1,25 @@
+﻿using Augustine.VietnameseCalendar.Core.LuniSolarCalendar;
+using System;
+using System.Windows.Forms;
+
+namespace VCalendar
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			LuniSolarDate<VietnameseLocalInfoProvider> lunarDate = LuniSolarCalendar<VietnameseLocalInfoProvider>.LuniSolarDateFromSolarDate(dateTimePicker1.Value);
+			label1.Text = (string.Format("{0:dd/MM/yyyy} = {1}", dateTimePicker1.Value, lunarDate.FullDayInfo));
+		}
+
+		private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
