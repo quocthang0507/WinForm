352a89bf891260b1b4765007fe2ce5b60f4497cf
La Quốc Thắng <quocthang0507@gmail.com>
2020-10-19 20:04:30 +0700

    Add new project via upload

---
+++ LapTrinhCSharp/HumanizerDemo/HumanizerDemo/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace HumanizerDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_output_vi = new System.Windows.Forms.TextBox();
+            this.txt_output_en = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Roman = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Number:";
+            // 
+            // txt_input
+            // 
+            this.txt_input.Location = new System.Drawing.Point(102, 82);
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(239, 20);
+            this.txt_input.TabIndex = 1;
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Location = new System.Drawing.Point(356, 80);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(171, 23);
+            this.btn_convert.TabIndex = 2;
+            this.btn_convert.Text = "Convert Number to Words";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "English:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Vietnamese:";
+            // 
+            // txt_output_vi
+            // 
+            this.txt_output_vi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txt_output_vi.Location = new System.Drawing.Point(102, 144);
+            this.txt_output_vi.Name = "txt_output_vi";
+            this.txt_output_vi.Size = new System.Drawing.Size(463, 20);
+            this.txt_output_vi.TabIndex = 5;
+            // 
+            // txt_output_en
+            // 
+            this.txt_output_en.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txt_output_en.Location = new System.Drawing.Point(102, 118);
+            this.txt_output_en.Name = "txt_output_en";
+            this.txt_output_en.Size = new System.Drawing.Size(463, 20);
+            this.txt_output_en.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Roman:";
+            // 
+            // txt_Roman
+            // 
+            this.txt_Roman.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txt_Roman.Location = new System.Drawing.Point(102, 170);
+            this.txt_Roman.Name = "txt_Roman";
+            this.txt_Roman.Size = new System.Drawing.Size(463, 20);
+            this.txt_Roman.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(92, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(198, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(204, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "USING HUMANIZER C#";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(465, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 208);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txt_Roman);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_output_en);
+            this.Controls.Add(this.txt_output_vi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_convert);
+            this.Controls.Add(this.txt_input);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "[C#] Using Lib Humanizer - https://laptrinhvb.net";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_output_vi;
+        private System.Windows.Forms.TextBox txt_output_en;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Roman;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ LapTrinhCSharp/HumanizerDemo/HumanizerDemo/Form1.cs
@@ -0,0 +1,35 @@
+﻿using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumanizerDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            var number = Convert.ToInt32(txt_input.Text);
+            txt_output_en.Text = number.ToWords().Transform(To.SentenceCase);
+            txt_output_vi.Text = number.ToWords(new System.Globalization.CultureInfo("vi")).Transform(To.SentenceCase);
+            try
+            {
+                txt_Roman.Text = number.ToRoman();
+            }
+            catch (Exception)
+            {                
+            }
+        }
+    }
+}
